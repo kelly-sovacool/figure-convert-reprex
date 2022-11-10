6c3b11d3153189ce2a9532ae2532ee78c4594e47
Kelly Sovacool <kellysovacool@gmail.com>
2022-11-09 23:16:33 -0500

    Overhaul for just-enough-python

--- workflow/scripts/plot_diversity.R
+++ workflow/scripts/plot_diversity.R
@@ -2,7 +2,7 @@ library(cowplot)
 library(dplyr)
 library(ggplot2)
 p <- ggplot() +
-    draw_label("fig 2: false pos", size = 80, angle = 45)
+    draw_label("fig 1: diversity", size = 80, angle = 45)
 
 ggsave(filename = snakemake@output[['tiff']], 
        plot = p,
